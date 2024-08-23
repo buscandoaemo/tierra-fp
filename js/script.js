8adba246a0ccafc84eb7b6b02bc1a14524865f48
Emanuel F <36936697+buscandoaemo@users.noreply.github.com>
2024-08-23 03:11:30 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,38 +6,25 @@ window.onscroll = function() {
     } else {
         navbar.classList.remove("scrolled");
     }
-
-    // Dynamic Active Class on Scroll
-    let sections = document.querySelectorAll("section");
-    let navLinks = document.querySelectorAll(".navbar-nav .nav-link");
-
-    let foundActive = false; // Variable para verificar si se encuentra una sección activa
-
-    sections.forEach((section, index) => {
-        let sectionTop = section.offsetTop - 50; // Ajusta según la altura de tu navbar
-        let sectionHeight = section.offsetHeight;
-        let scrollPosition = window.pageYOffset;
-
-        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-            navLinks.forEach(link => link.classList.remove("active"));
-            navLinks[index].classList.add("active");
-            foundActive = true; // Se encontró una sección activa
-        }
-    });
-
-    // Si no se encuentra ninguna sección activa, remover la clase 'active' de todos los enlaces
-    if (!foundActive) {
-        navLinks.forEach(link => link.classList.remove("active"));
-    }
 };
 
-// Ancla
+// Handle Active Class on Click
+let navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        navLinks.forEach(nav => nav.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
+
+// Smooth Scroll for Anchor Links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
 
         const target = document.querySelector(this.getAttribute('href'));
-        const offset = 40; // Ajusta este valor según la altura de tu navbar
+        const offset = 70; // Ajusta este valor según la altura de tu navbar
         const bodyRect = document.body.getBoundingClientRect().top;
         const elementRect = target.getBoundingClientRect().top;
         const elementPosition = elementRect - bodyRect;
@@ -50,7 +37,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Carrousel
+// Carousel Swipe Handling
 document.querySelectorAll('.carousel').forEach(carousel => {
     let startX, endX;
     
